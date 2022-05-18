dc97be6df910616860b9f68b94b8e690901b521d
JCPenne <jackcpalmer@icloud.com>
2022-05-18 10:54:43 -0700

    added head function to return first item of array

--- head.js
+++ head.js
@@ -3,3 +3,7 @@ const assertEqual = function (actual, expected) {
     ? console.log(`✅ Assertion passed: ${actual} === ${expected}`)
     : console.log(`❗️ Assertion failed: ${actual} !== ${expected}`);
 };
+
+const head = function (arr) {
+  return arr[0];
+};
