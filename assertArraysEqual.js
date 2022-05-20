65683a331cfad143165304a9ce9c040ac1d20f4e
JCPenne <jackcpalmer@icloud.com>
2022-05-20 10:58:02 -0700

    changes to assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -6,18 +6,24 @@ const assertArraysEqual = function(arr1,arr2) {
 
 const eqArrays = function (arg1,arg2) {
   let result = false
-  let checkLength = false;
+  let checkLength = false
+  let falseFlag = true
 
   arg1.length !== arg2.length
     ? checkLength = false
     : checkLength = true
     
+    // loop through each element of both arrays. If the elements do not match, turn the falseFlag to false.
+    // If falseFlag equals false turn result to false
   if (checkLength === true) {
     for (i in arg1) {
       arg1[i] !== arg2[i]
-      ? result = false
+      ? falseFlag = false
       : result = true
     }
+    falseFlag === false
+    ? result = false
+    : ''
   }
   return result;
 };
@@ -25,4 +31,4 @@ assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
 assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
 
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
+assertArraysEqual(["1", "2", "3"], ["1", '2', '3']) // => false
