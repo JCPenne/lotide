4d2d7604cd8712bcfd9a0503af7f059cf033f695
JCPenne <jackcpalmer@icloud.com>
2022-05-20 14:34:21 -0700

    change assertArraysEqual.js function order

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,9 +1,3 @@
-const assertArraysEqual = function(arr1,arr2) {
-  eqArrays(arr1,arr2)
-    ? console.log(`✅ Assertion passed: ${arr1} === ${arr2}`)
-    : console.log(`❗️ Assertion failed: ${arr1} !== ${arr2}`);
-}
-
 const eqArrays = function (arg1,arg2) {
   let result = false
   let checkLength = false
@@ -13,8 +7,6 @@ const eqArrays = function (arg1,arg2) {
     ? checkLength = false
     : checkLength = true
     
-    // loop through each element of both arrays. If the elements do not match, turn the falseFlag to false.
-    // If falseFlag equals false turn result to false
   if (checkLength === true) {
     for (i in arg1) {
       arg1[i] !== arg2[i]
@@ -27,6 +19,13 @@ const eqArrays = function (arg1,arg2) {
   }
   return result;
 };
+const assertArraysEqual = function(arr1,arr2) {
+  eqArrays(arr1,arr2)
+    ? console.log(`✅ Assertion passed: ${arr1} === ${arr2}`)
+    : console.log(`❗️ Assertion failed: ${arr1} !== ${arr2}`);
+}
+
+
 assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
 assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
 
