7683f65060b5bc3078fe91cec3ed04419771bf9a
JCPenne <jackcpalmer@icloud.com>
2022-05-23 11:38:29 -0700

    Added man verification to countLetters

--- countLetters.js
+++ countLetters.js
@@ -4,4 +4,19 @@ const assertEqual = function (actual, expected) {
     : console.log(`❗️ Assertion failed: ${actual} !== ${expected}`);
 };
 
-const countLetters = function(input)
+const countLetters = function (input) {
+  const result = {};
+
+  for (letters of input) {
+    if (result[letters]) {
+      result[letters] += 1;
+    } else {
+      result[letters] = 1;
+    }
+  }
+  return result;
+};
+
+const result1 = countLetters('LHL');
+
+assertEqual(result1['L'], 2);
