d2577f93c19606f379fa664bf08444be7869da79
JCPenne <jackcpalmer@icloud.com>
2022-06-06 18:24:20 -0700

    added mocha and chai, changed head.test.js to utilize M&C

--- test/head.test.js
+++ test/head.test.js
@@ -1,4 +1,11 @@
 const head = require('../head');
-const assertEqual = require('../assertEqual');
+const { assert } = require('chai');
 
-assertEqual(head([0, 1, 2]), 0);
+describe('#head', () => {
+  it('returns 1 for [1,2,3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns 5 for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+});
