4c66886d355eca97e8cac51c8bec6a82bdae5574
JCPenne <jackcpalmer@icloud.com>
2022-06-07 16:28:22 -0700

    changed findKey to use M&C added related test file

--- findKey.js
+++ findKey.js
@@ -1,34 +1,11 @@
-const assertEqual = function (actual, expected) {
-  actual === expected
-    ? console.log(`✅ Assertion passed: ${actual} === ${expected}`)
-    : console.log(`❗️ Assertion failed: ${actual} !== ${expected}`);
-};
-
 const findKey = function (obj, callback) {
+  resultArr = [];
   for (key in obj) {
     if (callback(obj[key])) {
-      return key;
+      resultArr.push(key);
     }
   }
+  return resultArr;
 };
 
-const obj1 = {
-  Matrix: { stars: 2 },
-  'Top Gun': { stars: 3 },
-  Inception: { stars: 5 },
-};
-
-const obj2 = {
-  John: {
-    Age: 32,
-  },
-  Jane: {
-    Age: 35,
-  },
-};
-
-results1 = findKey(obj1, x => x.stars === 5);
-results2 = findKey(obj2, x => x.Age > 32);
-
-// assertEqual(results1, 'Inception');
-// assertEqual(results2, 'Jane');
+module.exports = findKey;
---
+++ test/findKey.test.js
@@ -0,0 +1,17 @@
+const findKey = require('../findkey');
+const { assert } = require('chai');
+const obj1 = {
+  Matrix: { stars: 2 },
+  'Top Gun': { stars: 3 },
+  Inception: { stars: 5 },
+  Tenet: { stars: 5 },
+};
+
+describe('#findKey', () => {
+  it(`returns ['Inception','Tenet] for obj1, x => x.stars === 5`, () => {
+    assert.deepEqual(
+      findKey(obj1, x => x.stars === 5),
+      ['Inception', 'Tenet'],
+    );
+  });
+});
