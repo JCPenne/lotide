6e28b14edd55eed16a1ad33476091d097f6ac068
JCPenne <jackcpalmer@icloud.com>
2022-06-07 17:57:14 -0700

    Changed takeUntil to use M&C, added test file

---
+++ test/takeUntil.test.js
@@ -0,0 +1,31 @@
+const takeUntil = require('../takeUntil');
+const { assert } = require('chai');
+
+describe('#takeUntil', () => {
+  it(`returns [1,2,5] for [1,2,5,-1],x => x < 0`, () => {
+    assert.deepEqual(
+      takeUntil([1, 2, 5, -1], x => x < 0),
+      [1, 2, 5],
+    );
+  });
+});
+
+//Old objects and variables to test with original assert functions
+
+// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+// const data2 = [
+//   "I've",
+//   'been',
+//   'to',
+//   'Hollywood',
+//   ',',
+//   "I've",
+//   'been',
+//   'to',
+//   'Redwood',
+// ];
+// const results1 = takeUntil(data1, x => x < 0);
+// const results2 = takeUntil(data2, x => x === ',');
+
+// assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+// assertArraysEqual(results2, ["I've", 'been', 'to', 'Hollywood']);
