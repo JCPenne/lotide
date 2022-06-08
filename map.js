4971fa2743a0544f2229d363bd58f70421cbd432
JCPenne <jackcpalmer@icloud.com>
2022-06-07 17:41:38 -0700

    changed map.js to use M&C added test file

--- map.js
+++ map.js
@@ -1,12 +1,3 @@
-//Test objects
-const words = ['ground', 'control', 'to', 'major', 'tom'];
-const results = [70, 50, 100, 85, 12, 42];
-const lotr = [
-  { name: 'gandalf', species: 'wizard' },
-  { name: 'Frodo', species: 'hobbit' },
-  { name: 'Aragon', species: 'human' },
-];
-
 const map = function (array, callback) {
   const results = [];
   for (let item of array) {
@@ -14,13 +5,4 @@ const map = function (array, callback) {
   }
   return results;
 };
-//Test result to push to the original assertArraysEqual function
-const results1 = map(words, word => word[0]);
-const results2 = map(results, result => result > 70);
-const results3 = map(lotr, x => x.species);
-
-//Outdated test results against the original assertArraysEqual function
-
-// assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
-// assertArraysEqual(results2, [false, false, true, true, false, false]);
-// assertArraysEqual(results3, ['wizard', 'hobbit', 'human']);
+module.exports = map;
---
+++ test/map.test.js
@@ -0,0 +1,30 @@
+const map = require('../map');
+const { assert } = require('chai');
+
+describe('#map', () => {
+  it(`returns ['g','c'] for ['ground','control'],words, word => word[0]`, () => {
+    assert.deepEqual(
+      map(['ground', 'control'], word => word[0]),
+      ['g', 'c'],
+    );
+  });
+});
+
+//Test objects
+// const words = ['ground', 'control', 'to', 'major', 'tom'];
+// const results = [70, 50, 100, 85, 12, 42];
+// const lotr = [
+//   { name: 'gandalf', species: 'wizard' },
+//   { name: 'Frodo', species: 'hobbit' },
+//   { name: 'Aragon', species: 'human' },
+// ];
+
+//Test results to push to the original assertArraysEqual function
+// const results1 = map(words, word => word[0]);
+// const results2 = map(results, result => result > 70);
+// const results3 = map(lotr, x => x.species);
+
+//Old test results for orginal Assert functions
+// assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+// assertArraysEqual(results2, [false, false, true, true, false, false]);
+// assertArraysEqual(results3, ['wizard', 'hobbit', 'human']);
