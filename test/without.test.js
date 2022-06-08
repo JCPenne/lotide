27dae0d9430c5ad2fdb7447779af927e73eb50b4
JCPenne <jackcpalmer@icloud.com>
2022-06-07 17:58:36 -0700

    minor tweak to without.js

--- test/without.test.js
+++ test/without.test.js
@@ -6,3 +6,8 @@ describe('#without', () => {
     assert.deepEqual(without([1, 2, 3], [1]), [2, 3]);
   });
 });
+// Old assert function tests
+
+// assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+// assertArraysEqual(without(['1', '2', '3'], [1, 2, '3']), ['1', '2']);
+// assertArraysEqual(without([1, 2, 3, 4, 5], [1, 3, 5, 4]), [2]);
--- without.js
+++ without.js
@@ -2,6 +2,3 @@ const without = function (source, itemsToRemove) {
   return source.filter(keepNumber => !itemsToRemove.includes(keepNumber));
 };
 module.exports = without;
-// assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
-// assertArraysEqual(without(['1', '2', '3'], [1, 2, '3']), ['1', '2']);
-// assertArraysEqual(without([1, 2, 3, 4, 5], [1, 3, 5, 4]), [2]);
